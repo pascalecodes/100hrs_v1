fdd050bb7a67352942eb8704cd2ed4c7346cd543
pascalecodes <pdelauna@gmail.com>
2023-03-21 21:34:26 -0700

    add client side js script to listen and capture search term

--- public/js/search.js
+++ public/js/search.js
@@ -1,114 +1,27 @@
 // search.js
 
-
-
-
-
-
-
-
 const searchForm = document.querySelector('form');
-
-
-
 
 const searchInput = document.querySelector('#search-input');
 
-
-
-
 const searchButton = document.querySelector('#search-button');
-
-
-
-
 const searchResults = document.querySelector('#search-results');
 
-
-
-
-
-
-
-
 searchForm.addEventListener('submit', (event) => {
-
-
-
-
   event.preventDefault(); // prevent page refresh
-
-
-
-
   searchResults.innerHTML = ''; // clear previous search results
-
-
-
-
   const query = searchInput.value;
-
-
-
-
-
-
-
-
-  fetch(`/search?q=${query}`)
-
-
-
-
+  fetch(`/find?q=${query}`)
     .then(response => response.json())
-
-
-
-
     .then(results => {
-
-
-
-
       results.forEach(result => {
-
-
-
-
-        const article = `<a href="/articles/${result._id}"><h2>${result.title}</h2></a>
-
-
-
-
+        const post = `<a href="/posts/${result._id}"><h2>${result.title}</h2></a>
                          <p>${result.text.slice(0, 50)}...</p>`;
-
-
-
-
-        searchResults.innerHTML += article;
-
-
-
-
+        searchResults.innerHTML += post;
       });
-
-
-
-
+      console.log(query)
     })
-
-
-
-
     .catch(error => console.log(error));
-
-
-
-
-
-
-
-
   searchInput.value = ''; // clear input field
 
 
