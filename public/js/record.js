942ba7342ce3d985e4db85c903862a3b0c426e52
pascalecodes <pdelauna@gmail.com>
2023-04-02 11:50:08 -0700

    record client script to include saving the file name based on title

--- public/js/record.js
+++ public/js/record.js
@@ -21,18 +21,21 @@ window.onload = function () {
         document.querySelector('#message').innerText='Recording STOPPED, upload recording to Memwa';
         mediaRecorder.stop();
         let vidSave = document.getElementById('vid2')
+        document.getElementById("demo").innerHTML = "I have changed!";
         let blob = new Blob(parts, {
-            type: 'video/webm'
+            type: 'video/mp4'
         });
         //parts=[];
         const  url = URL.createObjectURL(blob);
         const a = document.createElement('a');
+        //const title= document.querySelector('#title').value
+        let fileName = document.getElementById('title').value
         document.body.appendChild(a);
         a.style = 'display: none';
         a.href = url;
         //a.download = vidSave.src;
-        a.download = "test.webm";
-        // let fileName = document.getElementById('title')
+        a.download = `${fileName}.mp4`;
+        // 
         // a.download = fileName
         a.click();
         vidSave.src = url;// create  a new location for file name also
