d5b55fda5a17e9e115457e0cfeecea9030853465
pascalecodes <pdelauna@gmail.com>
2023-05-14 21:08:38 -0700

    corrected the router to reflect update/id instead of profile/id

--- routes/main.js
+++ routes/main.js
@@ -15,7 +15,7 @@ router.get("/home", ensureAuth, homeController.getHome);
 router.get("/profile", ensureAuth, postsController.getProfile);
 router.get("/profile/edit", postsController.editProfile)
 router.post("/uploadAvatar", upload.single("avatar"), postsController.uploadAvatar)
-router.post("/update/:id", postsController.updateProfile)
+router.post("/update/:id", ensureAuth, postsController.updateProfile)
 
 router.get("/feed", ensureAuth, postsController.getFeed);
 router.get("/login", authController.getLogin);
